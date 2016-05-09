e32b82ecca4e43e3262e8c9235c60c2b3730811b
Vijay Swamidass <vswamidass@vswamidass-ltm3.internal.salesforce.com>
2016-05-09 12:47:39 -0700

    Fixed upsert to samples.

--- movedata.js
+++ movedata.js
@@ -1,6 +1,10 @@
 var data = require('promised-rest-client')({
     url: 'http://services.faa.gov/'
-})
+});
+
+var refocus = require('promised-rest-client')({
+    url: 'http://refocus.internal.salesforce.com/'
+});
 
 data.get({
     url: 'airport/list'
@@ -9,23 +13,83 @@ data.get({
     }
 }).then(function (resp) {
     for (i = 0; i < resp.length; i++) {
-        console.log(resp[i].CITY + '-' + resp[i].STATE);
+        var state = resp[i].STATE.replace(/\s/g, '');
+        var stateAbsolutePath = "Salesforce.Demos";
+        var stateSubject = {
+            isPublished: true
+            , name: state
+            , parentAbsolutePath: stateAbsolutePath
+        };
+
+        var city = resp[i].CITY.replace(/[^a-zA-Z]/g, '');
+        var cityAbsolutePath = stateAbsolutePath + "." + state;
+        var citySubject = {
+            isPublished: true
+            , name: city
+            , parentAbsolutePath: cityAbsolutePath
+        };
+
         var iata = resp[i].IATA;
-        
+        var airport = iata;
+        var airportAbsolutePath = cityAbsolutePath + "." + city;
+        var airportSubject = {
+            isPublished: true
+            , name: airport
+            , parentAbsolutePath: airportAbsolutePath
+        };
+
+        refocus.post({
+            url: 'v1/subjects'
+            , body: stateSubject
+        });
+
+        refocus.post({
+            url: 'v1/subjects'
+            , body: citySubject
+        });
+
+        refocus.post({
+            url: 'v1/subjects'
+            , body: airportSubject
+        });
+
         //if there is a delay then get the details
+        var hasDelay = "true";
         if (resp[i].DELAYCOUNT > 0) {
-            data.get({
-                url: 'airport/status/' + iata
-                , qs: {
-                    format: 'application/json'
-                }
-            }).then(function (resp) {
-                //push data to refocus
-                console.log(resp);
-            }).catch(function (err) {
-                console.error(err);
-            })
+            hasDelay = "false";
         }
+
+        var airportSubjectPath = airportSubject.parentAbsolutePath + "." + airportSubject.name;
+        var links = [{
+            "name": airportSubject.name + " Delays"
+            , "url": resp[i].URI + "?format=application/json"
+        }];
+        var sampleUpsertBody = {
+            "name": airportSubjectPath + "|AIRPORTDELAY"
+            , "relatedLinks": links
+            , "value": hasDelay
+        };
+        refocus.post({
+            url: 'v1/samples/upsert'
+            , body: sampleUpsertBody
+        });
+
+
+        /* data.get({
+             url: 'airport/status/' + iata
+             , qs: {
+                 format: 'application/json'
+             }
+         }).then(function (resp) {
+             //push data to refocus
+             
+             /*refocus.post({
+                 url: 'v1/samples'
+                 , body: {
+                     "name": "Salesforce.Demos.California.SanDiego.SAN|AIRPORTDELAY"
+                     , "value": "false"
+                 }
+             });*/
     }
 }).catch(function (err) {
     console.error(err);
